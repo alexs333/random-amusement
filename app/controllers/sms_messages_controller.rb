e3d21431ff46d5ea8d76bb874be9211b94c16884
alexs333 <aserdyuk@dius.com.au>
2015-03-25 15:02:48 +1100

    Base database and UI tweaks and configuration.

--- app/controllers/sms_messages_controller.rb
+++ app/controllers/sms_messages_controller.rb
@@ -1,9 +1,12 @@
 class SmsMessagesController < ApplicationController
+	before_action :require_login
+
 	def new
 		@sms_message = SmsMessage.new
 	end
 
 	def create
+		@sms_message = SmsMessage.new(params[:sms_message])
 		flash.now.notice = 'Message hand been sent!'
 		render action: 'new'
 	end
